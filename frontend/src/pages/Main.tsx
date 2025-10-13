b66d259bff63111d1ec08d05d0b1e937f18575ba
antonblaise <antoniusblaise@gmail.com>
2025-10-14 03:10:39 +0800

    Minor progress on frontend

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+import "./Main.css"
+
+// Arrays to store the carlines and stages
+const carlines = [
+	"T Line",
+	"S Line",
+	"S MOPF Line",
+	"RB Line"
+]
+
+const stages = [
+	{ to: "/stage1", title: "Stage 1" },
+	{ to: "/stage2", title: "Stage 2" },
+	{ to: "/stage3", title: "Stage 3" },
+	{ to: "/stage4", title: "Stage 4" },
+	{ to: "/stage5", title: "Stage 5" }
+]
+
 export default function Main() {
-	return <h1>Main Page</h1>;
+	return (
+		<div className="main-container">
+			{stages.map(stage => (
+				<Link to={stage.to} className="box-link" title={stage.title}>
+					<div className="box" />
+				</Link>
+			))}
+		</div>
+	);
 }
